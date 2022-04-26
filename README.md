4b342245a850c500dff81c7224e83a91bd5e0760
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-26 19:18:16 -0300

    chore: grayscale filter working

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">Welcome to Image Filter 👋</h1>
+<p>🔊  Program to modify the volume of an audio file.</p>
+
+## Install
+
+```sh
+git clone https://github.com/peguimasid/Image-Filter.git
+```
+
+## Compile
+
+```sh
+gcc *.c -o filter
+```
+
+## Run
+
+```sh
+./filter -r IMAGE.bmp REFLECTED.bmp
+```
+
+### Options:
+
+1. -b ➡ blur
+2. -g ➡ grayscale
+3. -r ➡ reflect
+4. -s ➡ sepia
+
+## Author
+
+👤 **Guilhermo Masid**
+
+- Github: [@peguimasid](https://github.com/peguimasid)
+- LinkedIn: [@guilhermo-masid](https://linkedin.com/in/guilhermo-masid-494677b8)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
--- helpers.c
+++ helpers.c
@@ -1,25 +1,42 @@
 #include "helpers.h"
 
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
+void	grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+	BYTE	red;
+	BYTE	green;
+	BYTE	blue;
+	BYTE	grayscale;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			red = image[i][j].rgbtRed;
+			green = image[i][j].rgbtGreen;
+			blue = image[i][j].rgbtBlue;
+			grayscale = (0.299 * red) + (0.587 * green) + (0.114 * blue);
+			image[i][j].rgbtRed = grayscale;
+			image[i][j].rgbtGreen = grayscale;
+			image[i][j].rgbtBlue = grayscale;
+		}
+	}
 }
 
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
+void	sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+	return ;
 }
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
+void	reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+	return ;
 }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
+void	blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+	return ;
 }
