ddcc1b95bab532c0e3550ecf466dced9b792595e
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-27 18:00:47 -0300

    feature: sepia filter working

--- helpers.c
+++ helpers.c
@@ -24,7 +24,49 @@ void	grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void	sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-	return ;
+	BYTE	red;
+	BYTE	green;
+	BYTE	blue;
+	int		tr;
+	int		tg;
+	int		tb;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			red = image[i][j].rgbtRed;
+			green = image[i][j].rgbtGreen;
+			blue = image[i][j].rgbtBlue;
+			tr = (0.393 * red) + (0.769 * green) + (0.189 * blue);
+			tg = (0.349 * red) + (0.686 * green) + (0.168 * blue);
+			tb = (0.272 * red) + (0.534 * green) + (0.131 * blue);
+			if (tr > 255)
+			{
+				image[i][j].rgbtRed = 255;
+			}
+			else
+			{
+				image[i][j].rgbtRed = tr;
+			}
+			if (tg > 255)
+			{
+				image[i][j].rgbtGreen = 255;
+			}
+			else
+			{
+				image[i][j].rgbtGreen = tg;
+			}
+			if (tb > 255)
+			{
+				image[i][j].rgbtBlue = 255;
+			}
+			else
+			{
+				image[i][j].rgbtBlue = tb;
+			}
+		}
+	}
 }
 
 // Reflect image horizontally
